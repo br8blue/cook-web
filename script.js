181a104b2c031e4a3722b71f4f2844dc0b360e58
br8blue <99920530+br8blue@users.noreply.github.com>
2024-12-26 18:40:42 -0500

    Code

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function sendRequest() {
+function submitRequest() {
     var email = 'neelsrini2012@gmail.com';
     var subject = 'New Recipe Request';
+    var body = 'Greetings Cook Web Team,';
+    var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
+
+    window.location.href = mailtoLink; 
+}
+
+function submitComplaint() {
+    var email = 'neelsrini2012@gmail.com';
+    var subject = 'Article Complaint';
     var body = 'Greetings Cook Web Team,';
     var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
 
